96b7ebd1686a6f9a375b8ea1e529275a1da7ab0a
jianghaiyusheng <958463867@qq.com>
2022-06-30 10:47:17 +0800

    自动切换solc版本bug修复和优化

--- core/utils/change_solc_version.py
+++ core/utils/change_solc_version.py
@@ -12,10 +12,11 @@ def change_solc_version(sol_path:str):
             versions.append(cur_version)
         else:
             versions.append(item)
+    print(f'当前solc版本：{cur_version}')
 
     # 获取sol文件需要的版本
     obj_version = ''
-    with open(sol_path,'r') as fp:
+    with open(sol_path,'r',encoding='utf-8') as fp:
         for line in fp.readlines():
             line = line.strip()
             if line.startswith('pragma'):
@@ -23,19 +24,25 @@ def change_solc_version(sol_path:str):
                 if line.startswith('solidity'):
                     line = line[8:-1].strip()
                     break
-        # 只考虑 = 和 ^ 的情况
-        prefix = line[0]
-        v = line[1:]
-        if prefix == '=':
-            if v in versions:
-                obj_version = v
-        if prefix == '^':
+        # 1. pragma solidity 0.6.9;
+        # 2. pragma solidity ^0.6.9;
+        # 3. pragma solidity >=0.6.9 <=0.6.9;
+        # 只考虑前面2中情况
+        if line.startswith('0'):
+            obj_version = line
+        elif line.startswith('^'):
+            versions.insert(0,cur_version)
             for item in versions:
-                if item.startswith(v[:3]) and item >= v:
+                if item.startswith(line[1:5]) and item >= line[1:]:
                     obj_version = item
                     break
+        else:
+            print(f'为考虑这种模式：{line}，可能需要人工设置')
+            return
+    print(f'需要solc版本：{obj_version}')
+
     # 切换版本
-    if obj_version == '':
-        print('未找到合适的solidity版本')
+    if  obj_version not in versions or obj_version == '':
+        print("未找到合适的solidity版本，需要先安装，")
     elif obj_version != cur_version:
         subprocess.call(f'solc-select use {obj_version}')
