e151c972750b1e13ba0a3d72748b60a2b3118667
jianghaiyusheng <958463867@qq.com>
2022-08-31 12:04:21 +0800

    处理链上代码文件名与合约名不相同的情况

--- core/utils/source_code.py
+++ core/utils/source_code.py
@@ -4,8 +4,6 @@ import os
 import requests
 from core.common.e import Chain, TokenType
 
-INFO = 'info'
-
 class SourceCode:
 
     def __init__(self, chain: Chain, addr: str, token_type: TokenType, token_name: str) -> None:
@@ -30,13 +28,15 @@ class SourceCode:
         })        
         raw_contract_info = ret.json()['result'][0]
         raw_source_info: str = raw_contract_info['SourceCode']
-        contract_file_name: str = raw_contract_info['ContractName'] + '.sol'
+        contract_name: str = raw_contract_info['ContractName']
+        contract_file_name: str = contract_name + '.sol'
 
         conf = ConfigParser()
         conf.add_section('info')
         conf.set('info','chain', self.chain.name)
         conf.set('info','address',self.addr)
         conf.set('info','code_path',self.code_path)
+        conf.set('info','contract_name', contract_name)
 
         if raw_source_info.startswith('{'):
             if raw_source_info.startswith('{{'):
@@ -45,9 +45,9 @@ class SourceCode:
             if 'sources' in mul_source_info:
                 mul_source_info = mul_source_info['sources']
 
+            conf.set('info','contract_path', self.get_file_pos_by_contract_name(mul_source_info,contract_name))
+
             for contract_path, source_info in mul_source_info.items():
-                if contract_path.endswith(contract_file_name):
-                    conf.set(INFO, 'contract_path', contract_path)
                 full_file_name = self.code_path+'/'+contract_path
 
                 temp_dir_path, _ = os.path.split(full_file_name)
@@ -56,11 +56,26 @@ class SourceCode:
                     
                 self.write_sol(full_file_name, source_info['content'])
         else:
-            conf.set(INFO, 'contract_path', contract_file_name)
+            conf.set('info', 'contract_path', contract_file_name)
             self.write_sol(self.code_path+'/'+contract_file_name, raw_source_info)
 
         with open(self.conf_path, 'w', encoding='utf-8') as fp:
             conf.write(fp)
+
+    def get_file_pos_by_contract_name(self,mul_source_info,contract_name:str)->str:
+        for contract_path in mul_source_info.keys():
+            if contract_name in contract_path:
+                return contract_path
+        for contract_path, source_info in mul_source_info.items():
+            source_content:str = source_info['content']
+            for line in source_content.splitlines():
+                line = line.lstrip()
+                if line.startswith('contract'):
+                    line = line[8:].lstrip()
+                    if line.startswith(contract_name):
+                        return contract_path
+        return ''
+
 
     def write_sol(self, file_path:str, content:str):
         with open(file_path,'w',encoding='utf-8') as fp:
