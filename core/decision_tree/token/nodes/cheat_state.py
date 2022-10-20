038d85b8c4b9f6223f344e6d6300c5c2e2d41a35
jianghaiyusheng <958463867@qq.com>
2022-10-20 14:22:13 +0800

    增加对总量上限可绕过的处理

--- core/decision_tree/token/nodes/cheat_state.py
+++ core/decision_tree/token/nodes/cheat_state.py
@@ -1,9 +1,28 @@
 from ..common.base_node import DecisionNode, NodeReturn
 from ..common.token import ERC20_E_view, TokenInfo
+from slither.core.solidity_types import ElementaryType
 
 class CheatState(DecisionNode):
 
     def check(self, token_info: TokenInfo) -> NodeReturn:
+        cap_states = [s for s in token_info.c.state_variables \
+            if isinstance(s.type, ElementaryType) \
+                and 'uint' in s.type.type \
+                    and s.name in ['maxSupply','cap']
+        ]
+        if cap_states:
+            mint_fs = token_info.get_mint_fs()
+            for mint_f in mint_fs:
+                params = [param for param in mint_f.parameters \
+                    if isinstance(param.type, ElementaryType) \
+                        and 'uint' in param.type.type\
+                            and not mint_f.is_reading_in_require_or_assert(param)
+                ]
+                if params:
+                    cap_names = ','.join([cap_s.name for cap_s in cap_states])
+                    param_names = ','.join([param.name for param in params])
+                    self.add_warn(f'{token_info.c.name} 合约中存在最大上限相关定义 {cap_names} ，但其mint方法 {mint_f.name} 中的参数 {param_names} 未进行校验')
+
         if token_info.is_erc20:
             totalSupply_state = token_info.state_map[ERC20_E_view.totalSupply]
             mint_fs = token_info.get_mint_fs()
