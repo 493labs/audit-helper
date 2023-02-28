fb99443ec918651f9fbc60d47b738798fffeafb1
jianghaiyusheng <958463867@qq.com>
2023-02-28 10:37:11 +0800

    删除多余代码

--- bytecode/util.py
+++ bytecode/util.py
@@ -22,24 +22,3 @@ def get_contract(solc_dir:str, contract_path:str, contract_name:str=None)->Solid
         change_solc_version(contract_path)
         contract = SolidityContract(contract_path, contract_name)
     return contract
-
-    world_state = WorldState()
-    creator = Account(hex(ACTORS.creator.value))
-    attacker = Account(hex(ACTORS.attacker.value))
-    world_state.put_account(creator)
-    world_state.put_account(attacker)
-
-    transaction = ContractCreationTransaction(
-        world_state=world_state,
-        gas_limit=8000000,
-        origin=ACTORS.creator,
-        caller=ACTORS.creator,
-        code=Disassembly(deployed_code),
-        call_data=None
-    )
-    global_state = transaction.initial_global_state()
-    global_state.transaction_stack.append((transaction,None))
-    global_state.world_state.constraints = []
-    global_states_final = exec(global_state)
-    assert len(global_states_final) == 1
-    return global_states_final[0].world_state, transaction.callee_account
